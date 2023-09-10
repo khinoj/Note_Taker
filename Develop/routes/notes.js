1ca1b5fcb8b635ea6fc80afe7cdfb01a671fc7a8
khinoj <kristopher.hinojosa@gmail.com>
2023-09-10 12:11:21 -0400

    update

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -2,12 +2,31 @@ const notes = require('express').Router();
 const { v4: uuidv4 } = require('uuid');
 const { readAndAppend, readFromFile } = require('../helpers/fsUtils');
 
-notes.get('/', (req, res) => 
-readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+notes.get('/', (req, res) =>
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
 );
 
+notes.post('/', (req, res) => {
 
+    const { title, text } = req.body;
 
+    if (title && text) {
+        const noteDB = {
+            title: title,
+            text: text,
+            note_id: uuidv4(),
+        };
 
+        readAndAppend(noteDB, './db/db.json');
+
+        const response = {
+            status: 'Note Saved',
+            body: noteDB,
+        };
+        res.json(response);
+    } else {
+        res.json('Error in posting');
+    }
+});
 
 module.exports = notes;
